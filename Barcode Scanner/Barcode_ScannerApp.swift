13e7c87f6fa124f6b63dd33f5211fde3415921cf
tugay emre yucedag <emrethedev@gmail.com>
2023-12-19 22:35:19 +0300

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Tugay Emre Yucedag on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
